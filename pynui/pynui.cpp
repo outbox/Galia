3045921ea6df628f1620efe719d822d34678208e
Maximo Martinez <max.maragu@gmail.com>
2011-09-06 10:49:57 -0300

    Add joint enum to python bindings

--- pynui/pynui.cpp
+++ pynui/pynui.cpp
@@ -4,7 +4,7 @@
 
 using namespace boost::python;
 
-struct Joint {
+struct JointTransform {
   object position, orientation;
   str repr() {
     std::string p = extract<std::string>(position.attr("__repr__")());
@@ -39,7 +39,7 @@ struct Nui {
         for(int j=0;j<joint_count;++j) {
           XnVector3D p = data.joints[i][j].position.position;
           float* o = data.joints[i][j].orientation.orientation.elements;
-          Joint joint = { 
+          JointTransform joint = { 
             vec3(p.X, p.Y, p.Z), 
             mat3(o[0], o[1], o[2], o[3], o[4], o[5], o[6], o[7], o[8])
           };
@@ -60,9 +60,40 @@ BOOST_PYTHON_MODULE(pynui)
     .def_readonly("users", &Nui::users)
   ;
   
-  class_<Joint>("Joint")
-    .def_readonly("position", &Joint::position)
-    .def_readonly("orientation", &Joint::orientation)
-    .def("__repr__", &Joint::repr)
+  class_<JointTransform>("JointTransform")
+    .def_readonly("position", &JointTransform::position)
+    .def_readonly("orientation", &JointTransform::orientation)
+    .def("__repr__", &JointTransform::repr)
+  ;
+  
+  enum_<XnSkeletonJoint>("joint")
+    .value("head", XN_SKEL_HEAD)
+    .value("neck", XN_SKEL_NECK)
+    .value("torso", XN_SKEL_TORSO)
+    .value("waist", XN_SKEL_WAIST)
+
+    .value("left_collar", XN_SKEL_LEFT_COLLAR)
+    .value("left_shoulder", XN_SKEL_LEFT_SHOULDER)
+    .value("left_elbow", XN_SKEL_LEFT_ELBOW)
+    .value("left_wrist", XN_SKEL_LEFT_WRIST)
+    .value("left_hand", XN_SKEL_LEFT_HAND)
+    .value("left_fingertip", XN_SKEL_LEFT_FINGERTIP)
+
+    .value("right_collar", XN_SKEL_RIGHT_COLLAR)
+    .value("right_shoulder", XN_SKEL_RIGHT_SHOULDER)
+    .value("right_elbow", XN_SKEL_RIGHT_ELBOW)
+    .value("right_wrist", XN_SKEL_RIGHT_WRIST)
+    .value("right_hand", XN_SKEL_RIGHT_HAND)
+    .value("right_fingertip", XN_SKEL_RIGHT_FINGERTIP)
+
+    .value("left_hip", XN_SKEL_LEFT_HIP)
+    .value("left_knee", XN_SKEL_LEFT_KNEE)
+    .value("left_ankle", XN_SKEL_LEFT_ANKLE)
+    .value("left_foot", XN_SKEL_LEFT_FOOT)
+    
+    .value("right_hip", XN_SKEL_RIGHT_HIP)
+    .value("right_knee", XN_SKEL_RIGHT_KNEE)
+    .value("right_ankle", XN_SKEL_RIGHT_ANKLE)
+    .value("right_foot", XN_SKEL_RIGHT_FOOT)
   ;
 }
