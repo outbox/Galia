338d3aba4e9bf3f85d574131678edaa864a952b0
Maximo Martinez <max.maragu@gmail.com>
2011-11-09 18:51:11 -0200

    Perspective arrow

--- app/arrow.py
+++ app/arrow.py
@@ -15,17 +15,23 @@ class Arrow(object):
     self.side = side
 
     maker = CardMaker('')
-    width = 0.2
-    height = width / 2 * base.camLens.getAspectRatio()
+    width = 0.15
+    height = width / 2
     maker.setFrame(0, width, -height/2, height/2)
-    self.node = render2d.attachNewNode(maker.generate())
+    self.node = render.attachNewNode(maker.generate())
     self.node.setTexture(loader.loadTexture('resources/arrow.png'))
     self.node.setTransparency(TransparencyAttrib.MAlpha, 1)
 
-    if side == Skeleton.left:
-      self.node.setScale(-1, 1, 1)
+    # Always draw cursor on top
+    self.node.setBin('fixed', 50)
+    self.node.setDepthWrite(False)
+    self.node.setDepthTest(False)
+
+    self.node.setHpr(180 if side == Skeleton.left else 0, -90, 0)
 
     self.set_time(0)
+
+    self.set_alpha(0.8)
 
   def update(self, users):
     if self.user not in users: return
@@ -42,18 +48,8 @@ class Arrow(object):
       return
 
     sign = 1 if self.side == Skeleton.right else -1
-    world_pos = Point3(pos.x - 0.5 + 0.15 * sign, -pos.y, 0)
-    mat = Mat4(base.cam.getMat())
-    mat.invertInPlace()
-    cam_pos = mat.xformPoint(world_pos)
-    pos = Point2()
-    project = base.camLens.project(Point3(cam_pos), pos)
-
-    if project: 
-      self.node.setPos(pos.x, 0, pos.y)
-      self.node.show()
-    else:
-      self.node.hide()
+    world_pos = Point3(pos.x - 0.5 + 0.17 * sign, -pos.y + 0.05, 0)
+    self.node.setPos(world_pos)
 
   def set_time(self, time):
     time = max(0, min(1, time))
@@ -82,6 +78,10 @@ class Arrow(object):
   def set_alpha(self, alpha):
     self.node.setColor(1,1,1,alpha)
 
+  def get_alpha(self):
+    return self.node.getColor().getW()
+
   def destroy(self):
     base.taskMgr.remove(self.animation_key)
-    interpolate(self.animation_key, self.set_alpha, lambda t: 1-t, 0.2, on_done=self.node.removeNode)
+    alpha = self.get_alpha()
+    interpolate(self.animation_key, self.set_alpha, lambda t: alpha*(1-t), 0.2, on_done=self.node.removeNode)
